4a3edd8581653f440ab6dab2f2488ec290c225fd
Cal Young <cyoung1024@gmail.com>
2019-09-13 16:36:20 +0200

    Update SettingsViewController

--- SpikeClientUI/SpikeClientSettingsViewController.swift
+++ SpikeClientUI/SpikeClientSettingsViewController.swift
@@ -13,30 +13,30 @@ import SpikeClient
 
 
 public class SpikeClientSettingsViewController: UITableViewController {
-
+    
     public let cgmManager: SpikeClientManager
-
+    
     public let glucoseUnit: HKUnit
-
+    
     public let allowsDeletion: Bool
-
+    
     public init(cgmManager: SpikeClientManager, glucoseUnit: HKUnit, allowsDeletion: Bool) {
         self.cgmManager = cgmManager
         self.glucoseUnit = glucoseUnit
         self.allowsDeletion = allowsDeletion
-
+        
         super.init(style: .grouped)
     }
-
+    
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = cgmManager.localizedTitle
-
+        
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44
         
@@ -59,25 +59,25 @@ public class SpikeClientSettingsViewController: UITableViewController {
             nav.notifyComplete()
         }
     }
-
+    
     // MARK: - UITableViewDataSource
-
+    
     private enum Section: Int, CaseIterable {
         case authentication
         case latestReading
         case delete
     }
-
+    
     override public func numberOfSections(in tableView: UITableView) -> Int {
-        return allowsDeletion ? Section.count : Section.count - 1
+        return allowsDeletion ? Section.allCases.count : Section.allCases.count - 1
     }
-
+    
     private enum LatestReadingRow: Int, CaseIterable {
         case glucose
         case date
         case trend
     }
-
+    
     override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch Section(rawValue: section)! {
         case .authentication:
@@ -88,13 +88,13 @@ public class SpikeClientSettingsViewController: UITableViewController {
             return 1
         }
     }
-
+    
     private lazy var glucoseFormatter: QuantityFormatter = {
         let formatter = QuantityFormatter()
         formatter.setPreferredNumberFormatter(for: glucoseUnit)
         return formatter
     }()
-
+    
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -102,27 +102,27 @@ public class SpikeClientSettingsViewController: UITableViewController {
         formatter.doesRelativeDateFormatting = true
         return formatter
     }()
-
+    
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch Section(rawValue: indexPath.section)! {
         case .authentication:
             let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.className, for: indexPath) as! SettingsTableViewCell
-
+            
             let service = cgmManager.spikeService
-
+            
             cell.textLabel?.text = LocalizedString("Credentials", comment: "Title of cell to set credentials")
             cell.detailTextLabel?.text = service.username ?? SettingsTableViewCell.TapToSetString
             cell.accessoryType = .disclosureIndicator
-
+            
             return cell
         case .latestReading:
             let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.className, for: indexPath) as! SettingsTableViewCell
             let glucose = cgmManager.latestBackfill
-
+            
             switch LatestReadingRow(rawValue: indexPath.row)! {
             case .glucose:
                 cell.textLabel?.text = LocalizedString("Glucose", comment: "Title describing glucose value")
-
+                
                 if let quantity = glucose?.quantity, let formatted = glucoseFormatter.string(from: quantity, for: glucoseUnit) {
                     cell.detailTextLabel?.text = formatted
                 } else {
@@ -130,7 +130,7 @@ public class SpikeClientSettingsViewController: UITableViewController {
                 }
             case .date:
                 cell.textLabel?.text = LocalizedString("Date", comment: "Title describing glucose date")
-
+                
                 if let date = glucose?.timestamp {
                     cell.detailTextLabel?.text = dateFormatter.string(from: date)
                 } else {
@@ -138,10 +138,10 @@ public class SpikeClientSettingsViewController: UITableViewController {
                 }
             case .trend:
                 cell.textLabel?.text = LocalizedString("Trend", comment: "Title describing glucose trend")
-
+                
                 cell.detailTextLabel?.text = glucose?.trendType?.localizedDescription ?? SettingsTableViewCell.NoValueString
             }
-
+            
             return cell
         case .delete:
             let cell = tableView.dequeueReusableCell(withIdentifier: TextButtonTableViewCell.className, for: indexPath) as! TextButtonTableViewCell
@@ -153,7 +153,7 @@ public class SpikeClientSettingsViewController: UITableViewController {
             return cell
         }
     }
-
+    
     public override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch Section(rawValue: section)! {
         case .authentication:
@@ -164,17 +164,17 @@ public class SpikeClientSettingsViewController: UITableViewController {
             return nil
         }
     }
-
+    
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch Section(rawValue: indexPath.section)! {
         case .authentication:
             let vc = AuthenticationViewController(authentication: cgmManager.spikeService)
             vc.authenticationObserver = { [weak self] (service) in
                 self?.cgmManager.spikeService = service
-
+                
                 self?.tableView.reloadRows(at: [indexPath], with: .none)
             }
-
+            
             show(vc, sender: nil)
         case .latestReading:
             tableView.deselectRow(at: indexPath, animated: true)
